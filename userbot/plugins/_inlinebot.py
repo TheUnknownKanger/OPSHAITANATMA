e5013bdc45e39079ee493910434d7593e64d71b3
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-20 10:48:43 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -19,7 +19,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© JARVISUserBot Help",
+                "© Op Shaitan ATMA Help",
                 text="{}\nTotal Plugins Loaded: {}".format(
                     query, len(CMD_LIST)),
                 buttons=buttons,
