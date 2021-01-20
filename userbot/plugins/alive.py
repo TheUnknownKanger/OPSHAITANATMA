905c1d947e674b1f61e6d385b5a63df19e5d4b32
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-20 10:44:16 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -4,8 +4,8 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/968ce4f27a57e50a801f9.jpg"
-pm_caption = "**🅻🅴🅶🅴🅽🅳 🅸🆂 🅾🅽🅻🅸🅽🅴**\n"
+PM_IMG = "https://telegra.ph/file/88bbdf42501f490fd5ef6.jpg"
+pm_caption = "**𝗢𝗣 𝗦𝗛𝗔𝗜𝗧𝗔𝗡 𝗔𝗧𝗠𝗔 🅸🆂 🅾🅽🅻🅸🅽🅴**\n"
 
 pm_caption += f"**𝕄𝕪 𝔹𝕠𝕤𝕤**            : {DEFAULTUSER}\n"
 
@@ -15,13 +15,13 @@ pm_caption += "Python Version           : 3.8.5\n"
 
 pm_caption += "Bot Version              : 7.0.9\n"
 
-pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ          : [ᴊᴏɪɴ](https://t.me/legendsupportchannel)\n"
+pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ          : [ᴊᴏɪɴ](https://t.me/shaitansupportchannel)\n"
 
-pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ            : [ᴊᴏɪɴ](https://t.me/legendsupportgroup)\n"
+pm_caption += "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ            : [ᴊᴏɪɴ](https://t.me/shaitansupportgroup)\n"
 
 pm_caption += "ʟɪᴄᴇɴꜱᴇ                  : AGPL-3.0  ʟɪᴄᴇɴꜱᴇ\n"
 
-pm_caption += "ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ             : LEGEND\n"
+pm_caption += "ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ             : 𝗦𝗛𝗔𝗜𝗧𝗔𝗡\n"
 
 pm_caption += "╔┓┏╦━╦┓╔┓╔━━╗\n║┗┛║┗╣┃║┃║X  X║\n║┏┓║┏╣┗╣┗╣╰╯║\n╚┛┗╩━╩━╩━╩━━╝"
 
