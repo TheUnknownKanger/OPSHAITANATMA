ef0e56b990d8c8f3a9bb925a3db31834067a90b2
Sycrox <77438385+TheUnknownKanger@users.noreply.github.com>
2021-01-20 10:31:48 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 print ("")
 print ("")
-print(""" Welcome To LEGEND String Generator By Jarvis""")
+print(""" Welcome To OP SHAITAN String Generator By SHAITAN""")
 print("""Kindly Enter Your Details To Continue ! """)
 
 API_KEY = '1524422'
@@ -12,7 +12,7 @@ while True:
    with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
       print("")
       session = client.session.save()
-      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Visit @legendsupportchannel For Any Help !")
+      client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` And Ask ShaitanFor Any Help !")
       print("You telegram String session successfully stored in your telegram, please check your Telegram Saved Messages ")
       print("Store it safe !!")
       print("Thanks For Choosing LEGEND Service. Hope that You will Have a Goodtime With Us!....)
